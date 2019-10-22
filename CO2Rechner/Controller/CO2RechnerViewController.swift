81c7a27f83a8dc36c54bb34f441fdc96dab414cd
Ralf Ebert <info@ralfebert.de>
2019-10-22 13:54:04 +0200

    Beispiel mit Verbrauch und Kraftstoffart

--- CO2Rechner/Controller/CO2RechnerViewController.swift
+++ CO2Rechner/Controller/CO2RechnerViewController.swift
@@ -6,6 +6,8 @@ class CO2RechnerViewController: UIViewController {
 
     // MARK: - Outlets
 
+    @IBOutlet var fahrzeugTypSegmentedControl: UISegmentedControl!
+    @IBOutlet var verbrauchTextField: UITextField!
     @IBOutlet var kmTextField: UITextField!
     @IBOutlet var resultLabel: UILabel!
 
@@ -18,9 +20,24 @@ class CO2RechnerViewController: UIViewController {
     // MARK: - Actions
 
     @IBAction func calculate() {
-        if let text = kmTextField.text, let km = Double(text), km > 0 {
-            let co2Kg = km * 0.1904
-            self.resultLabel.text = "CO₂-Emission: \(co2Kg)kg"
+
+        let formatter = NumberFormatter()
+        formatter.generatesDecimalNumbers = true
+        formatter.numberStyle = .decimal
+        
+        let multiplier: Decimal
+        switch self.fahrzeugTypSegmentedControl.selectedSegmentIndex {
+            case 0: // Benzin
+                multiplier = 23.8
+            case 1: // Diesel
+                multiplier = 26.5
+            default:
+                fatalError("Invalid fahrzeugTypSegmentedControl.selectedSegmentIndex")
+        }
+
+        if let km = formatter.number(from: kmTextField.text ?? "")?.decimalValue, let verbrauch = formatter.number(from: verbrauchTextField.text ?? "")?.decimalValue {
+            let co2Kg = km * (verbrauch * multiplier) / 1000
+            self.resultLabel.text = "CO₂-Emission: \(formatter.string(for: co2Kg)!)kg"
         } else {
             self.resultLabel.text = "-"
         }
